9663cf32141512447a3e5bf102aaf396ebd82713
Gabriel Stengel (Macbook Pro) <gstengel@princeton.edu>
2019-05-13 12:58:25 -0400

    more changes

---
+++ game-logic.js
@@ -0,0 +1,32 @@
+
+
+// GAME LOGIC
+function update(){
+	//stats.update();
+    //animate
+    rollingGroundSphere.rotation.x += rollingSpeed;
+    heroSphere.rotation.x -= heroRollingSpeed;
+    if(heroSphere.position.y<=heroBaseY){
+    	jumping=false;
+    	bounceValue=(Math.random()*0.04)+0.005;
+    }
+    heroSphere.position.y+=bounceValue;
+    heroSphere.position.x=THREE.Math.lerp(heroSphere.position.x,currentLane, 2*clock.getDelta());//clock.getElapsedTime());
+    bounceValue-=gravity;
+    if(clock.getElapsedTime()>treeReleaseInterval){
+    	clock.start();
+    	addPathTree();
+    	if(!hasCollided){
+			score+=2*treeReleaseInterval;
+			scoreText.innerHTML=score.toString();
+		}
+    }
+    doTreeLogic();
+    doExplosionLogic();
+}
+
+function GameLoop(){
+    requestAnimationFrame(GameLoop);
+    update();
+    render();
+}
--- main.js
+++ main.js
@@ -40,52 +40,23 @@ var scoreText;
 var score;
 var hasCollided;
 
-init();
-
 function init() {
-	// set up the scene
 	createScene();
-	//call game loop
-	update();
+	GameLoop();
 }
 
-
-
-
-function update(){
-	//stats.update();
-    //animate
-    rollingGroundSphere.rotation.x += rollingSpeed;
-    heroSphere.rotation.x -= heroRollingSpeed;
-    if(heroSphere.position.y<=heroBaseY){
-    	jumping=false;
-    	bounceValue=(Math.random()*0.04)+0.005;
-    }
-    heroSphere.position.y+=bounceValue;
-    heroSphere.position.x=THREE.Math.lerp(heroSphere.position.x,currentLane, 2*clock.getDelta());//clock.getElapsedTime());
-    bounceValue-=gravity;
-    if(clock.getElapsedTime()>treeReleaseInterval){
-    	clock.start();
-    	addPathTree();
-    	if(!hasCollided){
-			score+=2*treeReleaseInterval;
-			scoreText.innerHTML=score.toString();
-		}
-    }
-    doTreeLogic();
-    doExplosionLogic();
-    render();
-	requestAnimationFrame(update);//request next update
-}
-
-
+// DRAW SCENE
 function render(){
-    renderer.render(scene, camera);//draw
+    renderer.render(scene, camera);
 }
+
+
 function gameOver () {
-  //cancelAnimationFrame( globalRenderID );
+  //scancelAnimationFrame( globalRenderID );
   //window.clearInterval( powerupSpawnIntervalID );
 }
+
+
 function onWindowResize() {
 	//resize & align
 	sceneHeight = window.innerHeight;
