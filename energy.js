5a331ab23f99ed9604db23073c5c51b5c2e27f5d
lyrakatzman <lkatzman@princeton.edu>
2019-05-14 15:37:14 -0400

    Tried to add water

---
+++ energy.js
@@ -0,0 +1,28 @@
+function init(event){
+
+  // UI
+
+  fieldDistance = document.getElementById("distValue");
+  energyBar = document.getElementById("energyBar");
+  replayMessage = document.getElementById("replayMessage");
+  fieldLevel = document.getElementById("levelValue");
+  levelCircle = document.getElementById("levelCircleStroke");
+
+  resetGame();
+  createScene();
+
+  createLights();
+  createPlane();
+  createSea();
+  createSky();
+  createCoins();
+  createEnnemies();
+  createParticles();
+
+  document.addEventListener('mousemove', handleMouseMove, false);
+  document.addEventListener('touchmove', handleTouchMove, false);
+  document.addEventListener('mouseup', handleMouseUp, false);
+  document.addEventListener('touchend', handleTouchEnd, false);
+
+  loop();
+}
